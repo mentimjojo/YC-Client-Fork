430c731ff71bf336d2fea4e50b97e12c3c556a97
Tim <timnijborg@hotmail.nl>
2026-02-14 22:39:06 +0100

    Update WebSocket server URL in youcubeapi.lua

--- src/lib/youcubeapi.lua
+++ src/lib/youcubeapi.lua
@@ -32,7 +32,7 @@ end
 -- Look at the [Documentation](https://commandcracker.github.io/YouCube/) for moor information
 -- Contact the server owner on Discord, when the server is down
 local servers = {
-    "ws://mc.beltboys.nl:5000"
+    "wss://music.papi.beltboys.nl"
 }
 
 if settings then
