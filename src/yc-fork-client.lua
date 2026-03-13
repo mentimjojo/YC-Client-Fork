f0b1e671b377456ebaa60f16bb9fd9c34cb44085
mentimjojo <tim@mealbuddy.org>
2026-03-13 14:55:12 +0100

    Fix for not playing playlist songs

--- src/yc-fork-client.lua
+++ src/yc-fork-client.lua
@@ -398,7 +398,7 @@ local function play_audio(buffer, title, on_first_chunk)
 
                     if not peripheral.isPresent(audiodevice.name) then
                         audiodevice.dead = true
-                        debug_log(colors.red, "Tape drive disconnected: " .. (audiodevice.name or "unknown"))
+                        debug_log(colors.red, "Tape drive disconnected: " .. (audiodeodevice.name or "unknown"))
                         return
                     end
 
@@ -501,8 +501,9 @@ local function play_audio(buffer, title, on_first_chunk)
     end
 
     local function device_scanner()
-        while true do
+        while not eof do
             sleep(5)
+            if eof then return end
             local current_peripherals = peripheral.getNames()
             local current_speakers = {}
             local current_tapes = {}
@@ -711,7 +712,12 @@ local function play(url)
         while true do
             os.queueEvent("youcube:fill_buffers")
 
-            local event = os.pullEventRaw()
+            local event, p1, p2, p3 = os.pullEventRaw()
+
+            if event == "playback_done" then
+                os.queueEvent(event, p1, p2, p3)
+                break
+            end
 
             if event == "terminate" then
                 libs.serverapi.reset_term()
@@ -761,31 +767,55 @@ local function play(url)
 
     local function _hotkey_handler()
         while true do
-            local _, key = os.pullEvent("key")
+            local event, key = os.pullEvent()
 
-            if key == skip_key then
-                back_buffer[#back_buffer + 1] = url --finished playing, push the value to the back buffer
-                if #back_buffer > max_back then
-                    back_buffer[1] = nil --remove it from the front of the buffer
-                end
-                if not args.no_video then
-                    libs.serverapi.reset_term()
-                end
+            if event == "playback_done" then
+                os.queueEvent(event)
                 break
             end
 
-            if key == restart_key then
-                queue[#queue + 1] = url --add the current song to upcoming
-                if not args.no_video then
-                    libs.serverapi.reset_term()
+            if event == "key" then
+                if key == skip_key then
+                    back_buffer[#back_buffer + 1] = url --finished playing, push the value to the back buffer
+                    if #back_buffer > max_back then
+                        table.remove(back_buffer, 1) --remove it from the front of the buffer
+                    end
+                    if not args.no_video then
+                        libs.serverapi.reset_term()
+                    end
+                    break
                 end
-                restart = true
-                break
+
+                if key == restart_key then
+                    queue[#queue + 1] = url --add the current song to upcoming
+                    if not args.no_video then
+                        libs.serverapi.reset_term()
+                    end
+                    restart = true
+                    break
+                end
+
+                if key == back_key then
+                    queue[#queue + 1] = url --add the current song to upcoming
+                    local prev = table.remove(back_buffer)
+                    if prev then --nil/false check
+                        queue[#queue + 1] = prev --add previous song to upcoming
+                    end
+                    if not args.no_video then
+                        libs.serverapi.reset_term()
+                    end
+                    break
+                end
             end
         end
     end
 
-    parallel.waitForAny(fill_buffers, _play_media, _hotkey_handler)
+    local function play_media_wrapper()
+        _play_media()
+        os.queueEvent("playback_done")
+    end
+
+    parallel.waitForAll(fill_buffers, play_media_wrapper, _hotkey_handler)
 
     if data.playlist_videos then
         return data.playlist_videos
@@ -806,29 +836,10 @@ local function play_playlist(playlist)
     if args.shuffle then
         queue = shuffle_playlist(queue)
     end
-    while #queue ~= 0 do
-        local pl = table.remove(queue)
-
-        local function handle_back_hotkey()
-            while true do
-                local _, key = os.pullEvent("key")
-                if key == back_key then
-                    queue[#queue + 1] = pl --add the current song to upcoming
-                    local prev = table.remove(back_buffer)
-                    if prev then --nil/false check
-                        queue[#queue + 1] = prev --add previous song to upcoming
-                    end
-                    if not args.no_video then
-                        libs.serverapi.reset_term()
-                    end
-                    break
-                end
-            end
-        end
-
-        parallel.waitForAny(handle_back_hotkey, function()
-            play(pl) --play the url
-        end)
+    while #queue > 0 do
+        if restart then break end
+        local pl = table.remove(queue, 1)
+        play(pl)
     end
 end
 
